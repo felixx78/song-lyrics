601b0ed3d9f29367fc3b9c7327f9fb8a15cd643f
v1st78 <petrenkov605@gmail.com>
2023-12-18 23:05:34 +0200

    added localhost to cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors({origin: ['https://songlyrics-72bd9.web.app']}));
+app.use(cors({origin: ['https://songlyrics-72bd9.web.app', 'http://localhost:5173']}));
 
 app.use(express.json());
 
