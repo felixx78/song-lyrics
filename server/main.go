d79fb6d97d5e8873af226fbcc6e02b2b764dfe50
v1st78 <petrenkov605@gmail.com>
2023-12-20 12:46:43 +0200

    added cors

--- server/main.go
+++ server/main.go
@@ -16,6 +16,13 @@ func main(){
 
   r.HandleFunc("/api/artists/songs/{id}", handlers.ArtistSongs)
   r.HandleFunc("/api/artists/{id}", handlers.ArtistById)
+
+  headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"https://songslyrics.vercel.app","http://localhost:5173"}
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
+	// Wrap your router with the CORS middleware
+	corsRouter := handlers.CORS(headersOk, originsOk, methodsOk)(r)
   
   http.Handle("/", r)
   err := http.ListenAndServe(":3000", nil);
