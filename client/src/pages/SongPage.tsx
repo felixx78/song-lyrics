66fbcc5f0893f5a02d0cd54ba4c7ccf7b649077f
v1st78 <petrenkov605@gmail.com>
2023-12-20 18:47:05 +0200

    added condition rendering for date in SongPage.tsx

--- client/src/pages/SongPage.tsx
+++ client/src/pages/SongPage.tsx
@@ -122,23 +122,25 @@ function SongPage() {
                 </Link>
               </div>
 
-              <div className="mb-4 flex items-center gap-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-6 w-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-                  />
-                </svg>
-                <div> {song.release_date_for_display} </div>
-              </div>
+              {song.release_date_for_display && (
+                <div className="mb-4 flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+                    />
+                  </svg>
+                  <div> {song.release_date_for_display} </div>
+                </div>
+              )}
             </div>
 
             <div className="flex flex-wrap items-center gap-4">
@@ -166,7 +168,7 @@ function SongPage() {
             translatedLyrics={data.translatedLyrics}
           />
         ) : (
-          <div className="text-2xl text-center">No lyrics for this song</div>
+          <div className="text-center text-2xl">No lyrics for this song</div>
         )}
       </div>
     );
