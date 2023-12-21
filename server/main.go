af06a9410dc0f84e2bef2729a9a20fede47731bc
v1st78 <petrenkov605@gmail.com>
2023-12-21 11:37:05 +0200

    Refactor server/main.go

--- server/main.go
+++ server/main.go
@@ -4,19 +4,22 @@ import (
 	"fmt"
 	"net/http"
 	"songslyrics/handlers"
+
+	gorrilaHandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-  gorrilaHandlers "github.com/gorilla/handlers"
 )
 
 func main(){
   r := mux.NewRouter()
-  
-  r.HandleFunc("/api/songs/languages", handlers.Languages)
-  r.HandleFunc("/api/songs/search", handlers.Search)
-  r.HandleFunc("/api/songs/{id}", handlers.LyricsById)
 
-  r.HandleFunc("/api/artists/songs/{id}", handlers.ArtistSongs)
-  r.HandleFunc("/api/artists/{id}", handlers.ArtistById)
+  r = r.PathPrefix("/api").Subrouter()
+  
+  r.HandleFunc("/songs/languages", handlers.Languages)
+  r.HandleFunc("/songs/search", handlers.Search)
+  r.HandleFunc("/songs/{id}", handlers.LyricsById)
+
+  r.HandleFunc("/artists/songs/{id}", handlers.ArtistSongs)
+  r.HandleFunc("/artists/{id}", handlers.ArtistById)
 
   headersOk := gorrilaHandlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
   originsOk := gorrilaHandlers.AllowedOrigins([]string{"https://songslyrics.vercel.app", "http://localhost:5173"})
