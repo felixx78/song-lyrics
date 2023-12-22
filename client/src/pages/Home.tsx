a17721977cfbb427909e6c1b0c807dfd9f27a5a0
v1st78 <petrenkov605@gmail.com>
2023-12-22 07:35:42 +0200

    Added min height to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import SongSearch from "../components/SongSearch";
 
 function Home() {
   return (
-    <div className="flex h-[50vh] flex-col items-center justify-center px-6">
+    <div className="flex min-h-[50vh] flex-col items-center justify-center px-6">
       <h1 className="mb-2 text-4xl">Search</h1>
       <SongSearch py="2" />
     </div>
