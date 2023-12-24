9862c1be7fc54787bf179026a6c83fd2975410ea
v1st78 <petrenkov605@gmail.com>
2023-12-24 11:20:35 +0200

    Added not found error SongById.go

--- server/handlers/songbyid.go
+++ server/handlers/songbyid.go
@@ -35,6 +35,12 @@ func SongById(w http.ResponseWriter, r *http.Request){
     fmt.Println(err)
     return
   }
+
+  if resp.StatusCode != http.StatusOK {
+    http.Error(w, "Not Found", http.StatusNotFound)
+    return
+  }
+
   defer resp.Body.Close()
 
   var song GeniusResponse
