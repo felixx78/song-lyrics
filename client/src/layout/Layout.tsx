87b019199b459b35657ba4a8bafcb66321f30902
v1st78 <petrenkov605@gmail.com>
2023-12-24 08:51:06 +0200

    Added ScrollToTop

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import ScrollToTop from "../components/ScrollToTop";
 
 function Layout() {
   return (
     <div className="min-h-screen bg-[#35335C] font-semibold text-white">
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
