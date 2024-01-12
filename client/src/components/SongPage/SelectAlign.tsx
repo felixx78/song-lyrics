26ea49ea7bde218e658e6aa0e4f7f2b9c798c1ed
v1st78 <petrenkov605@gmail.com>
2024-01-13 00:11:29 +0200

    change bg color and padding SelectAlign

--- client/src/components/SongPage/SelectAlign.tsx
+++ client/src/components/SongPage/SelectAlign.tsx
@@ -33,12 +33,12 @@ const SelectAlign = ({ onChange }: { onChange: (align: string) => void }) => {
       <div
         className={`${
           isOpened ? "top-[110%]" : "invisible top-3/4 opacity-0"
-        } absolute left-0 z-20 w-full divide-y-2 divide-indigo-500 rounded border-2 border-indigo-500 bg-indigo-600 transition-all`}
+        } absolute left-0 z-20 w-full divide-y-2 divide-indigo-500 rounded border-2 border-indigo-500 transition-all`}
       >
         {["Left", "Center", "Right"].map((i) => (
           <button
             onClick={() => changeAlign(i)}
-            className="flex w-full items-center justify-between px-1 py-2 hover:bg-indigo-700 "
+            className="flex w-full items-center justify-between px-2 py-2 hover:bg-indigo-700 "
             key={i}
           >
             {i}
