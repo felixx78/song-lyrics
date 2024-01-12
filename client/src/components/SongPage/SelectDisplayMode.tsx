b6aecf1e37508048c4904b8e109c366b6cfa7b99
v1st78 <petrenkov605@gmail.com>
2024-01-12 17:58:35 +0200

    redesign SelectDisplayMode

--- client/src/components/SongPage/SelectDisplayMode.tsx
+++ client/src/components/SongPage/SelectDisplayMode.tsx
@@ -30,33 +30,35 @@ const SelectDisplayMode = ({
       <button
         ref={refButton}
         onClick={() => setIsOpened(!isOpened)}
-        className="rounded-md bg-indigo-500 px-8 py-1 active:bg-indigo-600"
+        className="rounded-md border-2 border-indigo-500 px-8 py-1"
       >
         Display
       </button>
-      {isOpened && (
-        <div className="absolute left-0 top-[105%] z-20 w-full divide-y-2 divide-indigo-400 rounded border-2 border-indigo-400 bg-indigo-500">
-          <button
-            onClick={() => changeDisplayMode("1")}
-            className="w-full pb-2 pt-3"
-          >
-            <div className="px-2">
-              <span className="mb-1 block h-1 bg-gray-400"></span>
-              <span className="block h-1 bg-gray-400"></span>
-            </div>
-          </button>
+      <div
+        className={`${
+          isOpened ? "top-[110%]" : "invisible top-3/4 opacity-0"
+        } absolute left-0 z-20 w-full divide-y-2 divide-indigo-500 rounded border-2 border-indigo-500 bg-indigo-600 transition-all duration-300`}
+      >
+        <button
+          onClick={() => changeDisplayMode("1")}
+          className="w-full pb-2 pt-3 hover:bg-indigo-700"
+        >
+          <div className="px-2">
+            <span className="mb-1 block h-1 bg-gray-400"></span>
+            <span className="block h-1 bg-gray-400"></span>
+          </div>
+        </button>
 
-          <button
-            onClick={() => changeDisplayMode("2")}
-            className="w-full pb-2 pt-3"
-          >
-            <div className="flex justify-between gap-2 px-2">
-              <span className="mb-1 block h-1 w-1/2 bg-gray-400"></span>
-              <span className="block h-1 w-1/2 bg-gray-400"></span>
-            </div>
-          </button>
-        </div>
-      )}
+        <button
+          onClick={() => changeDisplayMode("2")}
+          className="w-full pb-2 pt-3 hover:bg-indigo-700"
+        >
+          <div className="flex justify-between gap-2 px-2">
+            <span className="mb-1 block h-1 w-1/2 bg-gray-400"></span>
+            <span className="block h-1 w-1/2 bg-gray-400"></span>
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
