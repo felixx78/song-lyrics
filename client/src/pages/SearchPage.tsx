a33d465b6ee5e83e0c77f33b7bc08aaf65aa1aea
v1st78 <petrenkov605@gmail.com>
2024-04-11 21:28:03 +0300

    refactor SearchPage

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -28,27 +28,27 @@ function SearchPage() {
   return (
     <div className="container pb-8 pt-4">
       <h1 className="mb-4 text-2xl sm:text-4xl">
-        {data.length === 0 && !isLoading && "No"} Results for "{query.trim()}"
+        {!isLoading && data.length === 0 && !intObserver.current ? "No" : ""}{" "}
+        Results for "{query.trim()}"
       </h1>
 
       <div className="mx-auto grid max-w-[1300px] grid-cols-1 justify-items-center gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {!!data.length &&
-          data.map((i, index) => {
-            if (i.type === "song") {
-              return (
-                <div
-                  key={i.result.id}
-                  ref={index === data.length - 1 ? lastSongCard : null}
-                >
-                  <SongCard song={i.result} />
-                </div>
-              );
-            }
+        {data.map((i, index) => {
+          if (i.type === "song") {
+            return (
+              <div
+                key={i.result.id}
+                ref={index === data.length - 1 ? lastSongCard : null}
+              >
+                <SongCard song={i.result} />
+              </div>
+            );
+          }
 
-            return null;
-          })}
+          return null;
+        })}
 
-        {isLoading &&
+        {(isLoading || !data.length || !intObserver.current) &&
           Array.from({ length: 8 }).map((_, index) => (
             <SongCardSkeleton key={index} />
           ))}
