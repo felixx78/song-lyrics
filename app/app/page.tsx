4ffd60995b034acce8d13e5d781a8f43266eddc9
felixx78 <petrenkov605@gmail.com>
2024-12-04 22:01:25 +0200

    add home page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Search from "./components/Search";
 
 export default function Home() {
-  return <div className="">home</div>;
+  return (
+    <div className="pt-28 px-4">
+      <h1 className="text-center text-3xl mb-4 ">Search</h1>
+      <Search size="lg" />
+    </div>
+  );
 }
