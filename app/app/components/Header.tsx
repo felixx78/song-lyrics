a0384bcea29ef33a9f4f6365a4ef05c4bfba3dc1
felixx78 <petrenkov605@gmail.com>
2024-12-04 21:49:58 +0200

    add Search

--- app/app/components/Header.tsx
+++ app/app/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import clsx from "clsx";
 import { Lora } from "next/font/google";
+import { usePathname } from "next/navigation";
+import Search from "./Search";
+import Link from "next/link";
 
 const lora = Lora({
   subsets: ["latin"],
@@ -7,9 +12,14 @@ const lora = Lora({
 });
 
 function Header() {
+  const location = usePathname();
+
   return (
-    <header className="px-4 py-3">
-      <p className={clsx("font-medium text-xl", lora.className)}>song lyrics</p>
+    <header className="px-4 py-3 flex justify-between">
+      <Link href="/" className={clsx("font-medium text-xl", lora.className)}>
+        song lyrics
+      </Link>
+      {location !== "/" && <Search size="md" />}
     </header>
   );
 }
---
+++ app/app/components/Search.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { KeyboardEvent, MouseEvent, useEffect, useRef, useState } from "react";
+import { useDebounce } from "use-debounce";
+import MangifyingGlass from "../icons/MangifyingGlass";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Song from "../types/Song";
+import Link from "next/link";
+import clsx from "clsx";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  size: "md" | "lg";
+};
+
+function Search({ size }: Props) {
+  const router = useRouter();
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [text, setText] = useState("");
+  const [value, setValue] = useDebounce(text, 50);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null);
+
+  const { data } = useQuery({
+    queryKey: ["search", value],
+    queryFn: () =>
+      axios.get(`/api/search?q=${value}`).then((r) => r.data as Song[]),
+    enabled: !!value,
+  });
+
+  useEffect(() => {
+    const handleClick = (e: globalThis.MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        setIsOpen(false);
+      }
+      setHighlightedIndex(null);
+    };
+
+    document.addEventListener("click", handleClick);
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [ref]);
+
+  const handleLinkClick = (e?: MouseEvent<HTMLAnchorElement>) => {
+    setTimeout(() => {
+      setText("");
+      setValue("");
+      setHighlightedIndex(null);
+      setIsOpen(false);
+    }, 100);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (!data) return;
+
+    if (e.code === "Enter" && highlightedIndex) {
+      router.push(`/song/${data[highlightedIndex].id}`);
+      handleLinkClick();
+    } else if (e.code === "ArrowDown") {
+      if (highlightedIndex === null || highlightedIndex + 1 === data.length) {
+        setHighlightedIndex(0);
+      } else setHighlightedIndex(highlightedIndex + 1);
+    } else if (e.code === "ArrowUp") {
+      if (highlightedIndex === null || highlightedIndex - 1 === -1) {
+        setHighlightedIndex(data.length - 1);
+      } else setHighlightedIndex(highlightedIndex - 1);
+    }
+  };
+
+  return (
+    <div
+      ref={ref}
+      className={clsx(
+        "relative",
+        size === "lg" ? "mx-auto max-w-[500px]" : "max-w-[350px]"
+      )}
+    >
+      <div className="relative">
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="bg-black border placeholder-gray-400 border-gray-600 text-gray-200 pl-3 pr-9 py-1.5 rounded-md w-full"
+          spellCheck={false}
+          placeholder="search"
+          type="text"
+          onKeyDown={handleKeyDown}
+          onFocus={() => setIsOpen(true)}
+        />
+        <MangifyingGlass className="text-gray-400 absolute right-2 top-1/2 -translate-y-1/2" />
+      </div>
+
+      {isOpen && !!data?.length && (
+        <div className="bg-black border absolute w-full divide-y divide-gray-600 rounded-md border-gray-600 top-[120%]">
+          {data.map((i, index) => (
+            <Link
+              href={`/song/${i.id}`}
+              onClick={handleLinkClick}
+              className={clsx(
+                "px-2 py-1.5 block w-full hover:bg-gray-600 truncate text-left",
+                highlightedIndex === index && "bg-gray-600"
+              )}
+              key={i.id}
+            >
+              {i.title} - {i.artist_names}
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Search;
