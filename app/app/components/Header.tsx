859130f7edfc7c341d996e78ccb81f99b6e4f17e
felixx78 <petrenkov605@gmail.com>
2024-12-08 15:49:31 +0200

    fix logo tab border Header

--- app/app/components/Header.tsx
+++ app/app/components/Header.tsx
@@ -15,8 +15,11 @@ function Header() {
   const location = usePathname();
 
   return (
-    <header className="px-4 py-3 flex justify-between">
-      <Link href="/" className={clsx("font-medium text-xl", lora.className)}>
+    <header className="px-4 py-3 flex items-start justify-between">
+      <Link
+        href="/"
+        className={clsx("font-medium text-xl px-1", lora.className)}
+      >
         song lyrics
       </Link>
       {location !== "/" && <Search size="md" />}
