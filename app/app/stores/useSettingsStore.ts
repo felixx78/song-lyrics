e7d5f8166473040204444ffcb8f084e253177e2d
felixx78 <petrenkov605@gmail.com>
2024-12-09 12:45:23 +0200

    add language to settings

--- app/app/stores/useSettingsStore.ts
+++ app/app/stores/useSettingsStore.ts
@@ -3,9 +3,15 @@ import Settings, { Aligment, FontSize, ShowFirst } from "../types/Settings";
 import getLocalstorageItem from "../helpers/getLocalstorageItem";
 
 const useSettingsStore = create<Settings>((set) => ({
+  language: getLocalstorageItem("language", "en"),
   alignment: getLocalstorageItem<Aligment>("aligment", "left"),
   showFirst: getLocalstorageItem<ShowFirst>("showFirst", "translate"),
   fontSize: getLocalstorageItem<FontSize>("fontSize", "medium"),
+
+  setLanguage: (language: string) => {
+    set({ language });
+    localStorage.setItem("language", language);
+  },
 
   setAlignment: (alignment: Aligment) => {
     set({ alignment });
--- app/app/types/Settings.ts
+++ app/app/types/Settings.ts
@@ -3,9 +3,12 @@ export type FontSize = "small" | "medium" | "large";
 export type ShowFirst = "original" | "translate";
 
 type Settings = {
+  language: string;
   alignment: Aligment;
   showFirst: ShowFirst;
   fontSize: FontSize;
+
+  setLanguage: (language: string) => void;
   setAlignment: (alignment: Aligment) => void;
   setShowFirst: (showOriginalFirst: ShowFirst) => void;
   setFontSize: (fontSize: FontSize) => void;
