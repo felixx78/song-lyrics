31c67f1a5c955f1eb4d9dba02f13e794c531191e
felixx78 <petrenkov605@gmail.com>
2024-12-17 00:05:22 +0200

    update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,21 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "song lyrics",
+  description: "Translate your favorite song lyrics easily.",
+  keywords: ["song lyrics"],
+  openGraph: {
+    title: "song lyrics",
+    description: "Translate your favorite song lyrics easily.",
+    url: "https://song-lyrics.org",
+    images: [
+      {
+        url: "https://song-lyrics.org/_next/image?url=%2Fimage.jpg&w=640&q=75%201x,%20/_next/image?url=%2Fimage.jpg&w=1200&q=75",
+        width: 600,
+        height: 600,
+        alt: "Song Lyrics Translation App",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
