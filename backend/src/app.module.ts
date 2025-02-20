0cd30c17226eafe44c5679a92fdeca472948cc92
felixx78 <petrenkov605@gmail.com>
2025-02-20 23:41:16 +0200

    remove app controller and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GeniusApiModule } from './genius-api/genius-api.module';
 import { ConfigModule } from '@nestjs/config';
 import { SearchModule } from './search/search.module';
@@ -8,7 +6,5 @@ import { SongsModule } from './songs/songs.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), GeniusApiModule, SearchModule, SongsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
