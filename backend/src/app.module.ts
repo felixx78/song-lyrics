693be46e3b013a7b5d212eb70531824bcc6048c9
felixx78 <petrenkov605@gmail.com>
2025-02-20 22:04:31 +0200

    add ConfigModule and GeniusApiModule to app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GeniusApiModule } from './genius-api/genius-api.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), GeniusApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
