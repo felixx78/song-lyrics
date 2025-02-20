d63811be45493600fac14ad479cea5a359f45142
felixx78 <petrenkov605@gmail.com>
2025-02-20 21:43:43 +0200

    add Artist and Song type

---
+++ backend/src/types/Song.type.ts
@@ -0,0 +1,16 @@
+import Artist from './Artist.type';
+
+type Song = {
+  id: number;
+  title: string;
+  artist_names: string;
+  primary_artist: Artist;
+  featured_artists: Artist[];
+  song_art_image_thumbnail_url: string;
+  song_art_image_url: string;
+  album: { name: string } | null;
+  release_date_for_display: string;
+  url: string;
+};
+
+export default Song;
